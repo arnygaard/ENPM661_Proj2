6303fb12eb6d66350c5768ba1714cd6866b7265e
arnygaard <120501873+arnygaard@users.noreply.github.com>
2023-03-11 11:11:57 -0500

    Add files via upload
    Cleaned up comments

--- proj2.py
+++ proj2.py
@@ -9,8 +9,7 @@ import glob
 from IPython import display
 import re
 
-
-
+#defining action set
 def moveUp(x,y):
     next_loc = [x, y+1]
     return next_loc
@@ -35,7 +34,8 @@ def moveDRight(x,y):
 def moveDLeft(x,y):
     next_loc = [x-1, y-1]
     return next_loc
-    
+
+#defining moveset
 def moveset(map, start_node, end_node=0):
     next_node = []
     x, y = start_node
@@ -59,16 +59,21 @@ def moveset(map, start_node, end_node=0):
     for i in range(0, len(next_node)):
         tx = next_node[i][0]
         ty = next_node[i][1]
+        #making sure values stay within bounds
         if tx < 0 or ty < 0 or tx > 600 or ty > 250:
             temp_node.remove(next_node[i])
         if map[tx,ty,0] != 0:
             temp_node.remove(next_node[i])
     return temp_node
 
+#create empty map
 map_empty = np.zeros([600, 250, 3], dtype=np.uint8)
 map_copy = map_empty.copy()
 
+#5mm buffer as called for
 buffer = 5
+
+#creating square shape dimensions and assigning pixel values to map
 x_square_start = 450
 x_square_end = 550
 y_square_start = 50
@@ -77,12 +82,13 @@ for i in range(x_square_start, x_square_end):
     for j in range(y_square_start, y_square_end):
         map_empty[i,j,:] = [239,76,76]
 
+#plotting buffer zone
 for i in range(x_square_start-buffer, x_square_end+buffer):
     for j in range(y_square_start-buffer, y_square_end+buffer):
             if map_empty[i,j,0] != 239:
                 map_empty[i,j,:] = [200,16,16]
         
-
+#creating half circle dimensions and assigning pixel values to map
 x_hc = 300
 hc_r = 100
 y_hc = 120
@@ -91,26 +97,29 @@ for i in range(x_hc - hc_r, x_hc + hc_r):
         if (i - x_hc) **2 + (j - y_hc)**2 <= hc_r**2 and j > y_hc:
             map_empty[i,j,:] = [239,76,76]
 
+#plotting buffer zone
 for i in range(x_hc - hc_r - buffer, x_hc + hc_r + buffer):
     for j in range(y_hc - hc_r - buffer, y_hc + hc_r + buffer):
         if (i - x_hc) **2 + (j - y_hc)**2 <= (hc_r+buffer)**2 and j > y_hc - 5 and map_empty[i,j,0] != 239:
             map_empty[i,j,:] = [200,16,16]
 
+#creating diagonal wall dimensions and assigning pixel values to map
 x_wall_start = 80
 x_wall_end = 300
 y_wall_start = 0
 y_wall_end = 100
-
 for i in range(x_wall_start, x_wall_end):
     for j in range(y_wall_start, y_wall_end):
         if 0.25*i < j and 0.25*i + 25 > j: 
             map_empty[i,j,:] = [239,76,76]
 
+#plotting buffer zone
 for i in range(x_wall_start - buffer, x_wall_end + buffer):
     for j in range(y_wall_start - buffer, y_wall_end + buffer):
         if 0.25*i < j+buffer and 0.25*i + 25 > j-buffer and map_empty[i,j,0] != 239: 
             map_empty[i,j,:] = [200,16,16]
 
+#creating cost map of the map with included obstacles. -1 if within obstacle, 1E9 otherwise
 dim = (600,250)
 cost_map = np.zeros(dim)
 print(cost_map.shape)
@@ -121,6 +130,7 @@ for i in range(0, 600):
         else:
             cost_map[i,j] = -1
 
+#calculate cost to come for each node
 def CalcCost(child, parent):
     xc, yc = child
     xp, yp = parent
@@ -129,9 +139,12 @@ def CalcCost(child, parent):
     cost = round(np.sqrt(lx**2 + ly**2),1)
     return cost
 
+#backtracking function to find best path based on neighboring costs
+#find lowest neighboring cost, move to that node and repeat
 def backtrack(cost_map, start, goal):
     gx = goal[0]
     gy = goal[1]
+    #ensuring values stay within bounds
     if goal == [600, 250]:
         gx = gx - 1
         gy = gy - 1
@@ -201,12 +214,14 @@ def backtrack(cost_map, start, goal):
     path_nodes.reverse()
     return path_nodes
 
+#function to sort frames when processing output video
 # numbers = re.compile(r"(\d+)")
 # def numericalSort(value):
 #     parts = numbers.split(value)
 #     parts[1::2] = map(int, parts[1::2])
 #     return parts
 
+#assigning start and end points
 print('Enter start node x:')
 sx = int(input())
 print('Enter start node y:')
@@ -220,6 +235,7 @@ print('Goal Node:',gx, gy)
 Xs = [sx, sy]
 goal = [gx, gy]
 
+#checking for goalpoint or start point in obstacle
 if map_empty[gx,gy,0] != 0:
     print("goal lies in obstacle")
     sys.exit()
@@ -227,6 +243,7 @@ if map_empty[sx,sy,0] != 0:
     print("start lies in obstacle")
     sys.exit()
 
+#initializing lists and first cost of start node
 OpenList = []
 ClosedList = []
 # Xs = [0,0]
@@ -235,6 +252,7 @@ goal_state = 1
 tcost = []
 cost_map[Xs[0],Xs[1]] = 0
 
+#begin djikstra's algorithm
 OpenList.append(Xs)
 while OpenList and goal_state != 0:
 
@@ -258,10 +276,12 @@ while OpenList and goal_state != 0:
                     OpenList.append(item)
                     ClosedList.append(item)
     testing_node = []
-print("done")
+
+#find path from backtrack function
 path = backtrack(cost_map, Xs, goal)
 print("path:", path)          
 
+#plotting path
 for node in path:
     x = node[0]
     y = node[1]
@@ -273,6 +293,7 @@ plt.imshow(map)
 plt.gca().invert_yaxis()
 plt.show()
 
+#writing individual frames to folder that show djikstra search
 #ClosedList.append(goal)
 # ims = []
 # c = 0
@@ -301,71 +322,3 @@ plt.show()
 #             map_final = np.flip(map_final,0)
 #             cv.imwrite("images/Frame%d.jpg"%c, map_final)
 #             c = c+1
-# img_array = []
-# #path = r"C:\Users\Adam's PC\OneDrive\Desktop\661\images*.jpg"
-# for filename in sorted(glob.glob(r"C:/Users/Adam's PC/OneDrive/Desktop/661/images/*.jpg"), key=numericalSort):
-#     img = cv.imread(filename)
-#     height, width, layers = img.shape
-#     size = (width,height)
-#     img_array.append(img)
-
- 
-# out = cv.VideoWriter('results.avi',cv.VideoWriter_fourcc(*'DIVX'), 15, size)
- 
-# for i in range(len(img_array)):
-#     out.write(img_array[i])
-# out.release()
-
-# print(ims[1][1])
-# plt.imshow(ims[5])
-# plt.show()
-# cv.imshow("a",ims[1])
-# cv.waitKey(0)
-# out = cv.VideoWriter('results.avi', cv.VideoWriter_fourcc(*'DIVX'), 15, size)
-# for i in range(len(ims)):
-#     out.write(ims[i])
-# out.release()
-
-# Writer = animation.writers['ffmpeg']
-# writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
-
-# ani = animation.ArtistAnimation(fig, ims, interval=10, blit=True, repeat=False)
-# plt.suptitle('Adam Nygaard             Djisktra Search')
-# plt.gca().invert_yaxis()
-# ani.save('results.mp4', writer=writer)
-
-#ani.save('animation.mp4', progress_callback=lambda i, n: print(f'Saving frame {i} of {n}'))
-
-#plt.show()
-
-
-# ClosedList.append(goal)
-# fig, ax = plt.subplots()
-# ims = []
-# c = 0
-# for item in ClosedList:
-#     x = item[0]
-#     y = item[1]
-#     if x > 599:
-#         x = 599
-#     if y > 249:
-#         y = 249
-#     map_empty[x, y, :] = [119, 191, 88]
-#     map = np.transpose(map_empty, (1, 0, 2))
-#     im = ax.imshow(map, animated=True)
-#     ims.append([im])
-#     if item == goal:
-#         for node in path:
-#             i = node[0]
-#             j = node[1]
-#             if i > 599:
-#                 i = 599
-#             if j > 249:
-#                 j = 249
-#             map_empty[i,j,:] = [255, 255, 255]
-#             map = np.transpose(map_empty, (1, 0, 2))
-#             im = ax.imshow(map, animated=True)
-#             ims.append([im])
-
-
-
